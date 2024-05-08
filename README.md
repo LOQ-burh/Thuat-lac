bb6d011fd843a14173616b61f38493c6c6a109ff
LOQ-burh <nguyenminhquan042004@gmail.com>
2024-05-09 01:08:01 +0700

    1

--- README.md
+++ README.md
@@ -1,13 +1,21 @@
 ## request:
 
-> Mỗi nhà trọ các thông tin: mã phòng trọ, họ tên người thuê trọ, số CMND ngưòi thuê trọ, ngày tháng năm sinh người thuê trọ, chỉ số 
-> điện, chí số nước, đơn giá phòng trọ.  
+> Mỗi nhà trọ các thông tin: mã phòng trọ, họ tên người thuê trọ, số CMND ngưòi thuê trọ, ngày tháng năm sinh người thuê trọ, chỉ số điện, chí số nước, đơn giá phòng trọ.  
+
 > Tạo Queue chứa thông tin Nhà trọ bằng 2 cách: nhập từ bàn phim và đọc/ghi từ file $t x t$.  
+
 > Hiến thị tất cả các thông tin cua nhà trọ trong Queue.  
+
 > Xóa 1 phòng trọ trong Queue.  
+
 > Thêm 1 phòng trọ vào Queue.  
+
 > Tìm phòng trọ có chi số điện cao nhất.  
+
 > Tìm phòng trọ của người thuê trọ có họ tên " Tran Van A".  
-> Đếm số phòng trọ có đơn giá phòng trọ cao nhất.  
+
+> Đếm số phòng trọ có đơn giá phòng trọ cao nhất. 
+
 > Đếm số phòng trọ có người thuê trọ có năm $\sinh <2002$.  
+
 > Tính tổng chi số điện của tất cả các phòng trọ trong nhà trọ.
---
+++ index.cpp
@@ -0,0 +1,238 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct QNode { 
+    int data; 
+    QNode* next; 
+
+    QNode() 
+    { 
+        next = NULL; 
+    } 
+    QNode(int d) 
+    { 
+        data = d; 
+        next = NULL; 
+    } 
+    void input() {
+        cout << "data: ";
+		cin >> this->data;
+    }
+}; 
+
+struct NhaTro {
+	string maPhong;
+	string hoTen;
+	string ngaySinh; // Định dạng DD/MM/YYYY
+	string soCMND;
+	int chiSoDien;
+	int chiSoNuoc;
+	double donGia;
+
+	NhaTro* next;
+	NhaTro()
+	{
+		// cout << "ham khoi tao duoc goi!\n";
+		this->maPhong = this->hoTen = this->ngaySinh = this->soCMND = "";
+		this->chiSoDien = this->chiSoNuoc = 0;
+		this->donGia = 0;
+		next = NULL;
+	}
+	NhaTro(string maPhong, string hoTen, string ngaySinh, string soCMND, int chiSoDien, int chiSoNuoc, double donGia)
+	{
+		this->maPhong = hoTen;
+		this->hoTen = maPhong;
+		this->ngaySinh = ngaySinh;
+		this->soCMND = soCMND;
+		this->chiSoDien = chiSoDien;
+		this->chiSoNuoc = chiSoNuoc;
+		this->donGia = donGia;
+		next = NULL;
+	}
+	void input() {
+		cout << "nhap ma phong tro: ";
+		cin >> this->maPhong;
+		cin.ignore();
+		cout << "nhap ho ten nguoi thue tro: ";
+		getline(cin, this->hoTen);
+		cout << "nhap ngay thang nam sinh: ";
+		cin >> this->ngaySinh;
+		cout << "nhap chi so dien: ";
+		cin >> this->chiSoDien;
+		cout << "nhap chi so nuoc: ";
+		cin >> this->chiSoNuoc;
+		cout << "nhap don gia phong tro: ";
+		cin >> this->donGia;
+		cout << endl;
+	}
+	void toString() {
+		cout << "id: " << this->maPhong << endl;
+		cout << "ten: " << this->hoTen << endl;
+		cout << "ngay sinh: " << this->ngaySinh << endl;
+		cout << "chi so dien: " << this->chiSoDien << endl;
+		cout << "chi so nuoc: " << this->chiSoNuoc << endl;
+		cout << "don gia: " << fixed << setprecision(2) << this->donGia << endl;
+	}
+
+};
+
+class Queue {
+
+NhaTro nhaTro;
+
+public:
+	NhaTro *front, *rear;
+
+public:
+	Queue() {
+		front = NULL;
+		rear = NULL;
+	}
+
+	void enqueue(string maPhong, string hoTen, string ngaySinh, string soCMND, int chiSoDien, int chiSoNuoc, double donGia) {
+		NhaTro* temp = new NhaTro(maPhong, hoTen, ngaySinh, soCMND, chiSoDien, chiSoNuoc, donGia);
+		if (rear == NULL) {
+			front = rear = temp;
+			return;
+		}
+		rear->next = temp;
+		rear = temp;
+	}
+
+	//Xóa 1 phòng trọ trong Queue.
+	void dequeue() {
+		if (front == NULL) { return; }
+		NhaTro* temp = front;
+		front = front->next;
+
+		if (front == NULL) {
+			rear = NULL;
+		}
+
+		delete (temp);
+	}
+
+	void display() {
+		NhaTro* current = front;
+		while (current != nullptr) {
+			std::cout << "Ma Phong: " << current->maPhong << endl <<
+				"Ho Ten: " << current->hoTen << endl <<
+				"Ngay sinh: " << current->ngaySinh << endl <<
+				"Chi so dien: " << current->chiSoDien << endl <<
+				"Chi so nuoc: " << current->chiSoNuoc << endl <<
+				"Don gia: " << current->donGia << "\n\n";
+			current = current->next;
+		}
+	}
+	// Hàm tìm kiếm nhà trọ có chi số điện cao nhất
+	NhaTro* timChiSoDienCaoNhat() {
+		if (front == NULL) {
+			return NULL;
+		}
+
+		NhaTro* maxChiSoDienNhaTro = front;
+		NhaTro* current = front->next;
+
+		while (current != NULL) {
+			if (current->chiSoDien > maxChiSoDienNhaTro->chiSoDien) {
+				maxChiSoDienNhaTro = current;
+			}
+			current = current->next;
+		}
+
+		return maxChiSoDienNhaTro;
+	}
+	// Hàm tìm kiếm nhà trọ theo tên người thuê
+    NhaTro* timNhaTroTheoTen(const std::string& hoTen) {
+        NhaTro* current = front;
+        while (current != NULL) {
+            if (current->hoTen == hoTen) {
+                return current;
+            }
+            current = current->next;
+        }
+        return NULL;
+    }
+	// Hàm Đếm số phòng trọ có đơn giá phòng trọ cao nhất. 
+	int timSoPhongTroCoDonGiaCaoNhat() {
+		if (front == NULL) {
+			return NULL;
+		}
+		int count = 0;
+		NhaTro* maxDonGia = front;
+		NhaTro* current = front->next;
+
+		while (current != NULL) {
+			if (current->chiSoDien > maxDonGia->chiSoDien) {
+				maxDonGia = current;
+				count++;
+			}
+			current = current->next;
+		}
+
+		return count;
+	}
+};
+
+
+
+int main()
+{
+	Queue hangDoiNhaTro;
+
+	hangDoiNhaTro.enqueue("P001", "Tran Van A", "123456789", "01/01/1990",100, 150, 3000000.5);
+	hangDoiNhaTro.enqueue("P002", "Nguyen Van b", "123456789", "01/01/1990",100, 150, 3000000.5); // Tiếp tục nhập thông tin nhà trọ
+
+	cout << "Thong tin nhung nha tro trong queue: \n";
+	hangDoiNhaTro.display();
+
+	// Xóa phần tử khỏi hàng đợi sau khi xử lý xong
+	hangDoiNhaTro.dequeue();
+
+	// Hiển thị lại hàng đợi sau khi một nhà trọ được xóa
+	cout << "\nSau khi dequeue:\n";
+	hangDoiNhaTro.display();
+
+	NhaTro* nhaTroCoChiSoDienCaoNhat = hangDoiNhaTro.timChiSoDienCaoNhat();
+	if (nhaTroCoChiSoDienCaoNhat != nullptr) {
+		cout << "Phong tro co chi so dien cao nhat:\n";
+		cout << "Ma Phong: " << nhaTroCoChiSoDienCaoNhat->maPhong << endl <<
+		"Chi So Dien: " << nhaTroCoChiSoDienCaoNhat->chiSoDien << "\n";
+	}
+	else {
+		cout << "Queue khong co nha tro nao.\n";
+	}
+
+	std::string tenCanTim = "Tran Van A";
+    NhaTro* nhaTroCuaTranVanA = hangDoiNhaTro.timNhaTroTheoTen(tenCanTim);
+    if (nhaTroCuaTranVanA != NULL) {
+        std::cout << "Thong tin nha tro cua " << tenCanTim << "\n";
+        std::cout << "Ma Phong: " << nhaTroCuaTranVanA->maPhong << "\n";
+        // In ra thông tin khác của phòng trọ nếu muốn
+    } else {
+        std::cout << "Khong tim thay nha tro cua " << tenCanTim << "\n";
+    }
+
+	int soPhongTroDonGiaCaoNhat = hangDoiNhaTro.timSoPhongTroCoDonGiaCaoNhat();
+	if (soPhongTroDonGiaCaoNhat != 0) {
+        std::cout << "So phong tro co don gia cao nhat" << soPhongTroDonGiaCaoNhat << "\n";
+    } else {
+        std::cout << "Khong co so phong tro nao co don gia cao nhat" << "\n";
+    }
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
